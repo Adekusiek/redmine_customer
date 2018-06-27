b1b9e5ad463461e4b03011511206698ef82c318e
adekusiek <kawahara0114@gmail.com>
2018-06-27 14:54:52 +0900

    delete unnessary files

--- lib/supports/issue_patch.rb
+++
@@ -1,9 +0,0 @@
-module Supports
-	module IssuePatch
-		# has_one :customer, through: :issue_customer
-
-		def customer_address
-			customer ? customer.email : nil
-		end
-  end
-end
--- lib/supports/issue_query_patch.rb
+++
@@ -1,9 +0,0 @@
-module Supports
-	module IssueQueryPatch
-
-		def initialize_available_filters
-				super
-				add_available_filter "customer_address", :type => :text
-		end
-  end
-end
