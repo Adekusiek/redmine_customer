34cefc9cb56457206d4c6b8f4eab8ca1fd7d7335
adekusiek <kawahara0114@gmail.com>
2018-07-09 17:03:21 +0900

    erase cc:

--- app/models/enquete_mailer.rb
+++ app/models/enquete_mailer.rb
@@ -2,7 +2,7 @@ class EnqueteMailer < Mailer
 
   def enquete_send_mailer(issue, customer)
     @customer = customer
-    mail(to: customer.email, cc: "kaori.homma@ipg-automotive.com", bcc: "keisuke.kawahara@ipg-automotive.com", \
+    mail(to: customer.email, bcc: "keisuke.kawahara@ipg-automotive.com", \
        subject: "顧客満足度調査(サポート編)へのご協力のお願い CarMaker Support Enquete [#{issue.subject} ##{issue.id}]")
   end
 
--- app/models/enquete_reply_mailer.rb
+++ app/models/enquete_reply_mailer.rb
@@ -2,7 +2,7 @@ class EnqueteReplyMailer < Mailer
 
   def enquete_send_mailer(issue, customer)
     @customer = customer
-    mail(to: customer.email, cc: "kaori.homma@ipg-automotive.com", bcc: "keisuke.kawahara@ipg-automotive.com", subject: "#{issue.subject} : 顧客満足度調査への回答御礼")
+    mail(to: customer.email, bcc: "keisuke.kawahara@ipg-automotive.com", subject: "#{issue.subject} : 顧客満足度調査への回答御礼")
   end
 
 end
